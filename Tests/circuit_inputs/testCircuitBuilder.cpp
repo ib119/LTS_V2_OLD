23dd4db69d0738e6593d54613d33ba56a2767f65
ib119 <ib119@ic.ac.uk>
2020-05-12 22:18:49 +0200

    Fix indexing error cause when generating B when concidering voltage sources

--- Tests/circuit_inputs/testCircuitBuilder.cpp
+++ Tests/circuit_inputs/testCircuitBuilder.cpp
@@ -12,15 +12,42 @@
 #include "./../../main_project/resistor.hpp"
 #include "./../../main_project/resistor.cpp"
 
+#include "./../../main_project/currentSource.hpp"
+#include "./../../main_project/currentSource.cpp"
+
+#include "./../../main_project/voltageSource.hpp"
+#include "./../../main_project/voltageSource.cpp"
+
 using namespace std;
 
-int main(){
-    stringstream buffer;
+void onlyResistors(stringstream& buffer){
     buffer << "OnlyResistors" << endl;
     buffer << "R1 0 1 1" << endl;
     buffer << "R2 1 2 2" << endl;
     buffer << "R3 2 3 3" << endl;
     buffer << "r4 3 0 4" << endl;
+}
+
+void onlyCurrentSources(){
+
+}
+
+void onlyVoltageSources(){
+
+}
+
+void exampleCircuit1(stringstream& buffer){
+    buffer << "ExampleCircuit1" << endl;
+    buffer << "Vb 1 0 30" << endl;
+    buffer << "R1 1 2 5" << endl;
+    buffer << "R2 2 0 3" << endl;
+    buffer << "R3 2 0 10" << endl;
+    buffer << "Is 2 0 2" << endl;
+}
+
+int main(){
+    stringstream buffer;
+    exampleCircuit1(buffer);
 
     Circuit c{};
 
