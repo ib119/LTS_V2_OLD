4f864b318b726b79ef5b31853429dc8ec5d16065
nicholas-p1 <kontakt@nicholaspfaff.ch>
2020-05-13 08:32:52 +0100

    Added file for output functionality

---
+++ main_project/circuitOutput.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "circuit.hpp"
+#include "component.hpp"
+
+#include "voltageSource.hpp"
+#include "currentSource.hpp"
+#include "resistor.hpp"
+#include "capacitor.hpp"
+#include "inductor.hpp"
+
+using namespace std;
+
