1c77a573b2fa96f0e4a964300464fb19b4d4d36c
nicholas-p1 <kontakt@nicholaspfaff.ch>
2020-05-14 10:06:51 +0100

    Fixed enum problem by changing to enum struct

--- main_project/enums.hpp
+++ main_project/enums.hpp
@@ -4,15 +4,12 @@
 /*
     A collection of enums used in this project
 */
-struct componentType{
-    enum 
-    { 
-        conductanceSource = 0,
-        voltageSource = 1, 
-        currentSource = 2,
-        vcUpdatable = 3,
-        timeUpdatable = 4 
-    };
+enum struct componentType{
+    conductanceSource = 0,
+    voltageSource = 1, 
+    currentSource = 2,
+    vcUpdatable = 3,
+    timeUpdatable = 4 
 };
 
 #endif
