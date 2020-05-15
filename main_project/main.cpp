9a719150972dfb539fc4dceb25aaaca5b3d326f1
nicholas-p1 <kontakt@nicholaspfaff.ch>
2020-05-15 14:02:13 +0100

    Changed test in main

--- main_project/main.cpp
+++ main_project/main.cpp
@@ -12,12 +12,12 @@ using namespace std;
 void testCircuit(stringstream& buffer){
     buffer << "ExampleCircuit1" << endl;
     buffer << "V1 2 1 SIN(0 10 10)" << endl;
-    buffer << "R1 3 2 2k" << endl;
-    buffer << "L1 1 0 100m" << endl;
-    buffer << "R2 3 0 5k" << endl;
+    buffer << "R1 3 2 2000" << endl;
+    buffer << "L1 1 0 .1" << endl;
+    buffer << "R2 3 0 5000" << endl;
     buffer << "I1 4 3 .5" << endl;
-    buffer << "R3 4 0 10k" << endl;
-    buffer << "C1 4 0 1u" << endl;
+    buffer << "R3 4 0 10000" << endl;
+    buffer << "C1 4 0 .000001" << endl;
 }
 
 int main(int argc, char **argv){
