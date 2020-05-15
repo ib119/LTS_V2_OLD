8993ae99b664c9ba7ff363f963e1ae347867f345
ib119 <ib119@ic.ac.uk>
2020-05-15 18:15:18 +0200

    Add test cases and fix problem in get value test

--- Tests/circuit_inputs/testGetValue.cpp
+++ Tests/circuit_inputs/testGetValue.cpp
@@ -12,15 +12,15 @@
 using namespace std;
 
 int main(){
-    bool pass = false;
+    bool pass = true;
     // sets up a component to allow acces to get value function
     Resistor c{"R1", 0, 0, 1};
     
-    pass |= 10e-15 == c.getValue("10u");
-    pass |= 10 == c.getValue("10aksjdkjaskd");
-    pass |= 0.01== c.getValue("10mlamsldkasdasd");
-    pass |= 10000000 == c.getValue("10MEG");
-    pass |= 202401 == c.getValue(".202401G");
+    pass &= 10e-15 == c.getValue("10u");
+    pass &= 10 == c.getValue("10aksjdkjaskd");
+    pass &= 0.01== c.getValue("10mlamsldkasdasd");
+    pass &= 10000000 == c.getValue("10MEG");
+    pass &= 202401 == c.getValue(".202401G");
 
     if(pass){
         cout << "SUCCESS";
